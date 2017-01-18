f9fbbfd779d286716dbc6f566e63eb807af0c178
OmegaEinstein <894773488@qq.com>
2017-01-18 14:56:54 +0800

    add  get_validate of utils

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 import ConfigParser
 import base64
 import time
+import json
 
 work_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -25,4 +26,11 @@ def validate(key,fix_pwd):
     key = base64.b64decode(key)
     x = key.split('|')
     if len(x) != 5:
+        return json.dumps({'code':1, 'errmsg':"token参数不足"})
+    if t > int(x[4])+2*60*60:
+        return json.dumps({'code':1, 'errmsg':"登录已经过期"})
+    if fix_pwd == x[3]:
+        return json.dumps({'code':0, 'username':x[0], 'uid':x[1], 'r_id':x[2]})
+    else:
+        return json.dumps({'code':1, 'errmsg':"密码不正确"})
 
